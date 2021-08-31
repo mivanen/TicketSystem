a135e39358f7b1c62eb910df92bdb297e9d5b3ad
mivanen <mivanen@mail.ru>
2021-08-31 13:54:16 +0300

    stable 5

--- src/app/components/event/event.component.ts
+++ src/app/components/event/event.component.ts
@@ -28,9 +28,6 @@ events=[
 
 tickets=new Array();
 
-
-ticket_total:number=0;
-
 ticket_id:number=0;
 
 sold_tickets_count:number=1;
@@ -63,7 +60,13 @@ getAvaibleTickets(event_id:number)
   {
     if(value[1]==event_id)
     {
-      sold_tickets_count++;
+      
+       if(value[3]==true) 
+       {
+        sold_tickets_count++;
+       }
+      //console.log(value[3]);
+     
     }
   });
   
@@ -78,31 +81,30 @@ getAvaibleTickets(event_id:number)
 }
   
 
-  SellTicket(i:number)  
+  SellTicket(ticket_id:number)  
   {
 
-    let event_id= + this.events[i][0];
+    let event_id = +this.events[ticket_id][0];
 
     let event_ticket_avaible=this.getAvaibleTickets(event_id);
-     this.events[event_id-1][7]=event_ticket_avaible-1;
-     
-     if(event_ticket_avaible>0) 
-     {
 
-       if(event_ticket_avaible==1) {this.events[i][9]=false;}
-
-        this.ticket_id++;
-
-        let tickets_detail=new Array();
-        tickets_detail[0]=this.ticket_id;
-        tickets_detail[1]=event_id;
-        tickets_detail[2]=this.events[event_id-1][1];
-        tickets_detail[3]='true'; //Status
-        this.tickets.unshift(tickets_detail);
-
-     }
-     else this.events[i][9]=false;
+    if(event_ticket_avaible==1) this.events[event_id-1][9]=false;
+    
+    if(event_ticket_avaible>=1) 
+    {
+      this.ticket_id++;
+      let tickets_detail=new Array();
+      tickets_detail[0]=this.ticket_id;
+      tickets_detail[1]=event_id;
+      tickets_detail[2]=this.events[event_id-1][1];
+      tickets_detail[3]=true; //Status
+      this.tickets.push(tickets_detail);
+      //console.log(tickets_detail);
+    }
+    else this.events[event_id-1][9]=false;         
    
+    event_ticket_avaible=this.getAvaibleTickets(event_id);
+    this.events[event_id-1][7]=event_ticket_avaible;
   }
 
 
@@ -121,49 +123,89 @@ getAvaibleTickets(event_id:number)
 
     //alert(this.events[i][7]);
      if(this.events[i][7]>0) this.events[i][9]=true;
+     if(this.events[i][7]<0) this.events[i][9]=false;
 
 
   }
 
 
 
-  ReturnTicket(ticket_arr_id:number)
+  ReturnTicket(ticket_id:number)
   {
-    let ticket_id=this.tickets[ticket_arr_id][0];
-    let event_id = this.tickets[ticket_arr_id][1];
-    let event_title=this.events[event_id-1][1];
-
-    // alert('return Ticket id: '+ticket_id+' for event '+event_title+' (event id: '+event_id+')');
 
      
-     
-     let event_ticket_avaible=this.getAvaibleTickets(event_id);
-     this.events[event_id-1][7]=event_ticket_avaible+1;
-
-     //delete this.tickets[ticket_arr_id];
-
-     this.tickets[ticket_arr_id][3]='false';
+   // let event_id=0;
+   ticket_id--;
+    let event_id = this.tickets[ticket_id][1];
      
 
-     if(this.events[event_id-1][9]==false)
-     {
-       this.events[event_id-1][9]=true;
-        //alert('no pilets');
-     }
-
-     // alert(this.events[event_id-1][7]);
-     //if(this.events[event_id][7]>=0) this.events[event_id][9]=true;
-
-     //this.CheckTickets(event_id); 
+    this.tickets[ticket_id][3]=false;
+    //this.tickets[ticket_id][1]='';
+    let  event_ticket_avaible=this.getAvaibleTickets(event_id);
+    //console.log(event_ticket_avaible);
+    this.events[event_id-1][7]=event_ticket_avaible;
+    if(event_ticket_avaible>0) this.events[event_id-1][9]=true;
 
 
-    
 
-    
+    //let ticket_id=this.tickets[ticket_arr_id][0];
 
 
+    //let event_id = +this.tickets[ticket_id][1];
+    //let event_id = 1;
+    //let event_title=this.tickets[ticket_id][2];
+
+    //alert('return Ticket id: '+ticket_id+' (event id: '+event_id+')');
+     //alert('event id:'+event_id);
+     
+     
+  
+
+    //let aNumber  = parseInt(event_ticket_avaible);
+     //event_ticket_avaible+=1;
+
+     //delete this.tickets[ticket_id];
+//alert(this.tickets[ticket_id][3]);
+
+// let ticket_status=this.tickets[ticket_id][3];
+// alert(ticket_status);
+//     if(this.tickets[ticket_id][3]=='true')
+//     {
+      
+
+//       this.tickets[ticket_id][3]='false';
+//          //let event_ticket_avaible=this.getAvaibleTickets(event_id);
+//          let event_ticket_avaible=+this.events[event_id][7];
+//          event_ticket_avaible++;
+
+//          this.events[event_id][7]=event_ticket_avaible;
+//          if(event_ticket_avaible>0) this.events[event_id][9]='true';
+
+//     }
+
+     
+//      //alert('return Ticket id: '+ticket_id+'. Status: '+event_id+')');
+
+     
+//      if(this.events[event_id-1][9]==false)  this.events[event_id-1][9]=true;
 
   }
+
+  // GetTicketInfo(ticket_id:number, info_type:string)
+  // {
+  //   let response="";
+  //   switch (info_type) {
+  //     case "title":
+  //       response=this.tickets[ticket_id][2];
+  //       break;
+      
+  //     default:
+  //        response=this.tickets[ticket_id][0];
+  //       break;
+  //   }
+    
+  //   return(response);
+  // }
 
 
 
